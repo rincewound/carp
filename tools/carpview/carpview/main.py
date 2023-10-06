4b2cd4add4357e5137a3e679dfcaaee3391fecda
rincewound <aaargh@minddebugger.com>
2023-10-06 19:55:39 +0200

    Add 0MQ interface

--- tools/carpview/carpview/main.py
+++ tools/carpview/carpview/main.py
@@ -1,3 +1,5 @@
+import io
+import time
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
@@ -7,9 +9,12 @@ import json
 import sys
 import asyncio
 from asyncqt import QEventLoop
+import zmq
+from zmq.asyncio import Context
 
 class Ui(QtWidgets.QMainWindow):
     zeromq_enable = False
+    messages = []
 
     
     def __init__(self):
@@ -27,12 +32,16 @@ class Ui(QtWidgets.QMainWindow):
         self.actionLoad_Domain_File.triggered.connect(self.load_domain_file)
         self.actionLoad_aggregate_domain_file.triggered.connect(self.load_multi_domain_file)
         self.actionZeroMqToggle.triggered.connect(self.toggle_zeromq)
+        self.actionExit.triggered.connect(self.close)
 
         self.domains = {}
 
         self._load_multi_domain_file("./out.domain")
         self.parse_file("./data.bin")
-        self.setup_domain_list()       
+        self.setup_domain_list()      
+
+    def close(self):
+        Ui.zeromq_enable = False; 
 
     def toggle_zeromq(self):
         Ui.zeromq_enable = not Ui.zeromq_enable
@@ -82,63 +91,75 @@ class Ui(QtWidgets.QMainWindow):
 
     def clear_ui(self):
         self.tblEntries.model().clear()        
-        
+
+    def process_stream(self, stream):
+        while not len(stream.peek()) == 0:
+            # read timestamp 4 byte unsigned int:
+            timestamp = struct.unpack('I',stream.read(4))[0]
+            domain_id = struct.unpack('I',stream.read(4))[0]
+            message_id = struct.unpack('I',stream.read(4))[0]
+            num_args = struct.unpack('B',stream.read(1))[0]
+
+            print(f"found message {message_id} with {num_args} parameters")
+
+            args = []
+
+            for i in range(0, num_args):
+                type_id = struct.unpack('B',stream.read(1))[0]
+                print(f"type id {type_id}")
+                if type_id == 'I' or type_id == 73:      # int32
+                    value = struct.unpack('i',stream.read(4))[0]            
+                elif type_id == 'U':      # uint32
+                    value = struct.unpack('I',stream.read(4))[0]
+                elif type_id == 'B':    # uint8
+                    value = struct.unpack('b',stream.read(1))[0]
+                elif type_id == 'u':    # uint16
+                    value = struct.unpack('H',stream.read(2))[0]
+                elif type_id == 'F' or type_id == 70:    # float
+                    value = struct.unpack('f',stream.read(4))[0]
+                elif type_id == 'D':    # double
+                    value = struct.unpack('d',stream.read(8))[0]
+                elif type_id == 'S' or type_id == 83:    # string
+                    string_length = struct.unpack('H',stream.read(2))[0]
+                    value = stream.read(string_length).decode('utf-8')
+                else:
+                    print(f"unknown type {type_id}")
+                    value = None
+                pass
+
+                print(f" Read value {value} for parameter {i}")    
+                args.append(value)    
+            if domain_id != 0:
+                format_string = self.fetch_format_string(domain_id, message_id, num_args)                    
+            else:                    
+                format_string = args[0]
+                args.pop(0)                    
+
+            try:
+                format_string = format_string.format(*args)
+            except:
+                format_string = f"Invalid Format string '{format_string}' for {num_args} arguments"
+            
+            row = [QStandardItem(str(timestamp)), 
+                    QStandardItem(self.fetch_domain_name(domain_id)), 
+                    QStandardItem(format_string)]
+            
+            self.model.appendRow(row)
+        self.tblEntries.resizeColumnToContents(2)
 
     def parse_file(self, file: str):
         with open(file, 'rb') as f:
-            while not len(f.peek()) == 0:
-                # read timestamp 4 byte unsigned int:
-                timestamp = struct.unpack('I',f.read(4))[0]
-                domain_id = struct.unpack('I',f.read(4))[0]
-                message_id = struct.unpack('I',f.read(4))[0]
-                num_args = struct.unpack('B',f.read(1))[0]
+            self.process_stream(f)
+            
 
-                print(f"found message {message_id} with {num_args} parameters")
+    async def process_incoming_data(self):
+        while True:
+            if len(self.messages) > 0:
+                message = self.messages.pop(0)
+                stream = io.BufferedReader(io.BytesIO(message))
+                self.process_stream(stream)
+            await asyncio.sleep(1)
 
-                args = []
-
-                for i in range(0, num_args):
-                    type_id = struct.unpack('B',f.read(1))[0]
-                    print(f"type id {type_id}")
-                    if type_id == 'I' or type_id == 73:      # int32
-                        value = struct.unpack('i',f.read(4))[0]            
-                    elif type_id == 'U':      # uint32
-                        value = struct.unpack('I',f.read(4))[0]
-                    elif type_id == 'B':    # uint8
-                        value = struct.unpack('b',f.read(1))[0]
-                    elif type_id == 'u':    # uint16
-                        value = struct.unpack('H',f.read(2))[0]
-                    elif type_id == 'F' or type_id == 70:    # float
-                        value = struct.unpack('f',f.read(4))[0]
-                    elif type_id == 'D':    # double
-                        value = struct.unpack('d',f.read(8))[0]
-                    elif type_id == 'S' or type_id == 83:    # string
-                        string_length = struct.unpack('H',f.read(2))[0]
-                        value = f.read(string_length).decode('utf-8')
-                    else:
-                        print(f"unknown type {type_id}")
-                        value = None
-                    pass
-
-                    print(f" Read value {value} for parameter {i}")    
-                    args.append(value)    
-                if domain_id != 0:
-                    format_string = self.fetch_format_string(domain_id, message_id, num_args)                    
-                else:                    
-                    format_string = args[0]
-                    args.pop(0)                    
-
-                try:
-                    format_string = format_string.format(*args)
-                except:
-                    format_string = f"Invalid Format string '{format_string}' for {num_args} arguments"
-                
-                row = [QStandardItem(str(timestamp)), 
-                       QStandardItem(self.fetch_domain_name(domain_id)), 
-                       QStandardItem(format_string)]
-                
-                self.model.appendRow(row)
-        self.tblEntries.resizeColumnToContents(2)
 
 
     def fetch_domain_name(self, domain_id: int) -> str:
@@ -155,21 +176,32 @@ class Ui(QtWidgets.QMainWindow):
 
 
     async def zeromq_task():
-        while True:
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+        ctx = zmq.asyncio.Context()
+        sock = ctx.socket(zmq.REP)
+        print("Binding to tcp://*:5555")
+        sock.bind("tcp://*:5555")
+        while True:            
             if Ui.zeromq_enable is False:                
                 print("ZeroMQ is disabled")  
             else:
-                print("ZeroMQ is enabled")
+                print("ZeroMQ, receive some bytes")
+                data = await sock.recv()
+                print("send some bytes")
+                await sock.send_string("meh")
+                print("received some bytes: ", data)
+                Ui.messages.append(data)
             await asyncio.sleep(1)
 
 
 def run():
     app = QtWidgets.QApplication(sys.argv)
-    #window = Ui()
-    #app.exec_()
     loop = QEventLoop(app)
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     asyncio.set_event_loop(loop)
-    with loop:
-        w = Ui()
-        w.show()
-        loop.run_until_complete(w.zeromq_task())
+    
+    #with loop:
+    w = Ui()
+    w.show()        
+    asyncio.get_event_loop().create_task(w.process_incoming_data())
+    asyncio.get_event_loop().run_until_complete(Ui.zeromq_task())
